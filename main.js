ed0625980f429ac05159171727428022397fa8df
Zhuraveles <149628254+Zhuraveles@users.noreply.github.com>
2024-04-11 09:14:03 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    //Swiper
+  const swiper1 = new Swiper(".swiper1", {
+    slidesPerView: 1,
+    parallax:true,
+    speed:1000,
+    spaceBetween: 16,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  const swiper2 = new Swiper(".swiper2", {
+    slidesPerView: 1,
+    parallax:true,
+    speed:1000,
+    spaceBetween: 16,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  const swiper3 = new Swiper(".swiper3", {
+    slidesPerView: 1,
+    parallax:true,
+    speed:1000,
+    spaceBetween: 16,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  
+  const swiper4 = new Swiper(".swiper4", {
+    slidesPerView: 1,
+    parallax:true,
+    speed:1000,
+    spaceBetween: 16,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+});
